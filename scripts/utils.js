4f30889f686d68862e5804756a58ed531601116b
AdSchl2E <adam.schlee@insa-lyon.fr>
2024-12-10 09:10:12 +0100

    Ajout API TriplyDB

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,6 +1,7 @@
 // Constants
 const DBPEDIA_API = 'https://dbpedia.org/sparql'
 const WIKIDATA_API = 'https://query.wikidata.org/sparql'
+const TRIPLY_DB_API = 'https://api.triplydb.com/datasets/academy/pokemon/services/pokemon/sparql'
 
 // Functions
 function getQueryUrl(api, query) {
